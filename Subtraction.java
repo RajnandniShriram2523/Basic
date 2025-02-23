552a5fb0489ee536e6bf1736b9d7624402d9251d
Rajnandni <rajnandnishriram@gmail.com>
2025-02-23 15:13:44 +0530

    Suubtraction

---
+++ Subtraction.java
@@ -0,0 +1,12 @@
+public class Subtraction
+{
+	public static void main(String x[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Two Values=");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a-b;
+		System.out.println("Subtraction="+c);
+	}
+} 
